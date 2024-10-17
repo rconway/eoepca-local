c754dea5082bf839bf44a8677fbe084bfa3ce8a6
Richard Conway <richard@rconway.co.uk>
2024-10-17 15:22:42 +0100

    Add cluster issuers

--- certificate-manager/deploy.sh
+++ certificate-manager/deploy.sh
@@ -20,13 +20,13 @@ if [ "${ACTION}" = "apply" ]; then
   kubectl -n cert-manager rollout status --watch deployment cert-manager
   kubectl -n cert-manager rollout status --watch deployment cert-manager-cainjector
   kubectl -n cert-manager rollout status --watch deployment cert-manager-webhook
-  # kubectl -n ingress-nginx rollout status --watch deployment ingress-nginx-controller
+  kubectl -n ingress-nginx rollout status --watch daemonset nginx-ingress-controller
 fi
 
-# let status=1
-# while [ $status -ne 0 ]; do
-#   echo "Waiting to deploy Cluster Issuers..."
-#   kubectl ${ACTION} -f cluster-issuers.yaml 2>/dev/null
-#   let status=$?
-#   if [ $status -ne 0 ]; then sleep 5; fi
-# done
+let status=1
+while [ $status -ne 0 ]; do
+  echo "Waiting to deploy Cluster Issuers..."
+  kubectl ${ACTION} -f cluster-issuers.yaml 2>/dev/null
+  let status=$?
+  if [ $status -ne 0 ]; then sleep 5; fi
+done
